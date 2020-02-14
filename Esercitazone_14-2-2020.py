42f832fc44d472c6561723e63d3108ece03d20ba
guglielmo84 <rocco.guglielmo84@gmail.com>
2020-02-14 19:46:52 +0100

    Esercitazione 14-02-2020

---
+++ Esercitazone_14-2-2020.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 18:53:26 2020
+
+@author: user
+"""
+
+nomi=('Alessandro','Biagio','Claudia','Daniela','Emma')
+congnomi=('Abate','Bianchi','Corsini','Donati','Elia')
+
+print ('La struttura dati che usere ora è il dizionario. Il dizionario è un arrey associativo.  Accedere al contenuto di una cella mediante chiave')
+atomic_num = {'Dummy' : 0, 'H':1,'He':2, 'h':3}
+
+print (atomic_num['H'])
+
+atomic_num['C']=6
+
+
+print ("lo slicing 'affettare' significa prendere una sottoprzione") 
+
+print("Struttura dati chiamati SET, deriva dal concetto matemati di insieme")
+print('Il SET non puo avere dulicati')
+print ('il comando set applicato a una lista la ripulisce dai duplicati')
+
+
+
+print('*******************************************************************')
+
+print('Assegnamento puo essere SEMPLICE o AUMENTATO')
